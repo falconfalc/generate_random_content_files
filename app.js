16a01d3ebda5c102bcee0e4cc91ed5ebb064b206
Cristian Marica <Cristian.Marica@windriver.com>
2018-08-02 16:25:48 +0300

    Some syntax fixes

--- app.js
+++ app.js
@@ -1,62 +1,72 @@
 /*jshint esversion: 6*/
 
-var fs = require("fs");
-var path = require("path");
+(()=> {
+    "use strict";
 
-var params = {
-    minChars : 1500,
-    maxChars : 9500,
-    numOfFiles : 30,
-    pathForFiles : "./test_files"
-};
+    let fs = require("fs");
+    let path = require("path");
 
-function getRandomRange(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
-}
+    let params = {
+        minChars : 1500,
+        maxChars : 9500,
+        numOfFiles : 30,
+        pathForFiles : "./test_files"
+    };
 
-function generateStr(strLength){
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for(var i=0; i < strLength; i++){
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    function getRandomRange(min, max){
+        return Math.floor(Math.random() * (max - min) + min);
     }
 
-    return text;
-}
+    function generateStr(strLength){
+        let text = "";
+        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-function formatText(text, nChar){
-    var ret = [], i;
+        for(let i=0; i < strLength; i++){
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
 
-    for(i = 0; i < text.length; i += nChar){
-        ret.push(text.substr(i, nChar));
+        return text;
     }
 
-    return ret;
-}
+    function formatText(text, nChar) {
+        let ret = [], i;
 
-if(fs.existsSync(params.pathForFiles)){
-    fs.readdir(params.pathForFiles, (err, files) => {
-        if(err) throw error;
+        for(i = 0; i < text.length; i += nChar){
+            ret.push(text.substr(i, nChar));
+        }
 
-        files.forEach((file,key) =>{
-            fs.unlink(path.join(params.pathForFiles, file), err => {
-                if(err) throw err;
+        return ret;
+    }
+
+    if(fs.existsSync(params.pathForFiles)) {
+        fs.readdir(params.pathForFiles, (err, files) => {
+            if(err) {
+                throw err;
+            }
+
+            files.forEach((file,key) => {
+                fs.unlink(path.join(params.pathForFiles, file), (err) => {
+                    if(err) {
+                        throw err;
+                    }
+                });
             });
         });
-    });
-}else{
-    fs.mkdir(params.pathForFiles, err => {
-        if(err) throw err;
-    });
-}
-
-for(i = 1; i <= params.numOfFiles; i++){
-    var text = generateStr(getRandomRange(params.minChars, params.maxChars));
-    fs.writeFileSync(params.pathForFiles+"/test_file_"+i+".txt", formatText(text,120).join('\r\n'));
-
-    if(i === params.numOfFiles){
-        console.log(params.numOfFiles + " files were generated");
-        process.exit();
+    } else {
+        fs.mkdir(params.pathForFiles, (err) => {
+            if(err) {
+                throw err;
+            }
+        });
     }
-}
+
+    for(let i = 1; i <= params.numOfFiles; i++) {
+        var text = generateStr(getRandomRange(params.minChars, params.maxChars));
+        fs.writeFileSync(params.pathForFiles + "/test_file_" + i + ".txt", formatText(text, 120).join("\r\n"));
+
+        if(i === params.numOfFiles) {
+            console.log(params.numOfFiles + " files were generated");
+            process.exit();
+        }
+    }
+})();
